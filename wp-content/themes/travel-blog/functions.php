c6ddf36e04a0356ad1e926ef74f0fd8fb6d5972b
DWproduction5 <dwproduction5@DWproduction5.local>
2018-07-27 10:14:37 -0500

    google fonts added and header styles updated

--- wp-content/themes/travel-blog/functions.php
+++ wp-content/themes/travel-blog/functions.php
@@ -4,6 +4,8 @@ function site_styles(){
     wp_enqueue_style( 'normalize', get_stylesheet_directory_uri() . '/css/normalize.css' );
     wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
     wp_enqueue_style('site_style_css', get_stylesheet_uri());
+    wp_enqueue_style( 'raleway-font', 'https://fonts.googleapis.com/css?family=Raleway:900');
+    wp_enqueue_style( 'libre-baskerville-font', 'https://fonts.googleapis.com/css?family=Libre+Baskerville:400,700');
 
     wp_enqueue_script( 'jquery');
     wp_enqueue_script( 'bootstarpJS', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), '3.3.7', true);
