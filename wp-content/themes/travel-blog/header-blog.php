dab6b69ce576df465e9e57f6a81ddf300f2b8e0e
DWproduction5 <dwproduction5@DWproduction5.local>
2018-07-30 09:46:09 -0500

    Blog page settings

---
+++ wp-content/themes/travel-blog/header-blog.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <?php  wp_head();?>
+</head>
+<body <?php body_class() ?>>
+
+<?php $featured = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );?>
+<?php $featuredImage = $featured[0]; ?>
+<header class="site-header" style="background-image: url(<?php echo $featuredImage ?>)">
+    <nav class="main-navigation">
+    <div class="container">
+            <div class="row">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-span="false">
+                         <span class="sr-only">Toggle Naviagtion</span>
+                         <span class="icon-bar"></span>
+                         <span class="icon-bar"></span>
+                         <span class="icon-bar"></span>
+                    </button>
+                    
+                    <img src="<? echo get_stylesheet_directory_uri() ?>/img/logo.png" class="img-responsive">
+                </div>
+                <div class="navbar-right">
+                    <?php 
+                        wp_nav_menu( array(
+                            'theme_location' => 'headerMenu',
+                            'container_id' => 'navbar',
+                            'container_class' => 'collapse navbar-collapse',
+                            'menu_class' => 'nav navbar-nav navbar-right'
+                        ));
+                    ?>
+                </div>
+            </div><!--row-->
+        </div><!--container-->
+    </nav><!--.main-navigation-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="page-title">
+                    <?php
+                        $blog_page = get_option('page_for_posts');
+                        $title = get_the_title($blog_page);
+                    ?>
+                    <h1 class="title">
+                        <span><?php echo $title;?></span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+    
--- wp-content/themes/travel-blog/index.php
+++ wp-content/themes/travel-blog/index.php
@@ -1,4 +1,4 @@
-<?php  get_header();?>
+<?php  get_header('blog');?>
 
     <section class="container content">
         <div class="row">
