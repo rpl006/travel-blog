34adc2512714595dd8ffb6559b1399d6199f11ca
DWproduction5 <dwproduction5@DWproduction5.local>
2018-07-30 11:21:37 -0500

    single.php settings and some styles to blockquote

---
+++ wp-content/themes/travel-blog/single.php
@@ -0,0 +1,16 @@
+<?php  get_header();?>
+
+    <section class="container content">
+        <div class="row">
+            <?php 
+                while (have_posts()) {
+                    the_post();
+                    the_content();
+                }
+            ?>
+
+        </div>
+    </section>
+
+
+<?php  get_footer();?>
