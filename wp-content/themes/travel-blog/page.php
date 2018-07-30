ca70f8aa93a27d00c123fe6fb12f6960d3fb4394
DWproduction5 <dwproduction5@DWproduction5.local>
2018-07-30 10:14:52 -0500

    conditional setup for contact page

--- wp-content/themes/travel-blog/page.php
+++ wp-content/themes/travel-blog/page.php
@@ -2,12 +2,27 @@
 
     <section class="container content">
         <div class="row">
+            <!--check for contact Page-->
+            <?php 
+                if (is_page( 'contact' )) { ?>
+                    <div class="col-sm-8 col-sm-offset-2">
+                       <?php while (have_posts()) {
+                        the_post();
+                        the_content();
+                        }
+                        ?>
+                    </div>
+                <?php }
+            ?>
+
             <?php 
                 while (have_posts()) {
                     the_post();
                     the_content();
                 }
             ?>
+            
+
         </div>
     </section>
 
