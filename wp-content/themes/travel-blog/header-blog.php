942717d4cf4ba361c08716ed09d0ef2689fff2f9
DWproduction5 <dwproduction5@DWproduction5.local>
2018-07-30 12:43:23 -0500

    setting changed for page titles

--- wp-content/themes/travel-blog/header-blog.php
+++ wp-content/themes/travel-blog/header-blog.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    
     <?php  wp_head();?>
 </head>
 <body <?php body_class() ?>>
--- wp-content/themes/travel-blog/header.php
+++ wp-content/themes/travel-blog/header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes();?>>
 <head>
-    <meta charset="utf-8" />
+    <meta charset="<?php bloginfo('charset'); ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    
     <?php  wp_head();?>
 </head>
 <body <?php body_class() ?>>
